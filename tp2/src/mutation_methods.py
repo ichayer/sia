c00d876739c865eb027fea216050e0c08df53099
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2023-04-16 00:22:51 -0300

    Mutation methods

--- tp2/src/mutation_methods.py
+++ tp2/src/mutation_methods.py
@@ -5,54 +5,42 @@ from .individual import Individual
 
 # Mutations methods don't return anything. They work in place with the population variable.
 
-def gen(population: list[Individual], generation: int) -> None:
-    return 1
+# This is a general method that receives the color_ratios gene and applies 
+def mutate_gene(color_ratio):
+    change = 10
+    s = random.choice([1, -1])
+    return max(min(color_ratio + change * s, 100), 0)
 
 
-def limited_multigen(population: list[Individual], generation: int) -> None:
-    return 2
+def gen(individual: Individual, pm: float) -> None:
+    if random.random() < pm:
+        index_to_mutate = random.randrange(0, len(individual.color_ratios))
+        individual.color_ratios[index_to_mutate][1] = mutate_gene(individual.color_ratios[index_to_mutate][1])
+        individual.normalize()
 
 
-def uniform_multigen(population: list[Individual], generation: int) -> None:
-    return 3
+def limited_multigen(individual: Individual, pm: float) -> None:
+    amount_to_mutate = random.randint(0, len(individual.color_ratios))
+    chosen = [(i < amount_to_mutate) for i in range(amount_to_mutate)]
+    random.shuffle(chosen)
+    for i in range(len(individual.color_ratios)):
+        if chosen[i] and random.random() < pm:
+            individual.color_ratios[i][1] = mutate_gene(individual.color_ratios[i][1])
+    individual.normalize()
 
 
-# Complete Mutation
-# A random sign is chosen (to subtract or add). If it is possible to carry out that operation, it is done, otherwise, none is done.
-# Up to half of the genes are chosen at random (inside while). Then it goes through all the genes not chosen inside the while (inside for)
+def uniform_multigen(individual: Individual, pm: float) -> None:
+    for i in range(len(individual.color_ratios)):
+        if random.random() < pm:
+            individual.color_ratios[i][1] = mutate_gene(individual.color_ratios[i][1])
+    individual.normalize()
 
-def complete(population: list[Individual], generation: int) -> None:
-    r = random.uniform(0, 100)
-    probability = max(100 / math.pow(2, generation),
-                      2)  # TODO: bring these variables (prob and change) to the config (and remove generation parameter if possible)
-    # If generation > 1024, pow(2,1024) doesn't work.
-    change = max(100 / math.pow(2, generation), 5)
-    if probability >= r:
-        for individual in population:
-            sum_ratios = 0
-            randomly_chosen = 0
-            chosen = [False] * len(individual.color_ratios)
 
-            # Up to half of the genes are chosen at random
-            while randomly_chosen <= len(individual.color_ratios) / 2:
-                r = random.randint(0, len(individual.color_ratios) - 1)
-                if not chosen[r]:
-                    randomly_chosen += 1
-                    s = random.choice([1, -1])
-                    if 0 < individual.color_ratios[r][1] + change * s < 100:
-                        individual.color_ratios[r][1] += change * s
-                    sum_ratios += individual.color_ratios[r][1]
-                    chosen[r] = True
-
-            # Then it goes through all the genes not chosen inside the previous while
-            for i in range(len(individual.color_ratios)):
-                if not chosen[i]:
-                    s = random.choice([1, -1])
-                    if 0 < individual.color_ratios[i][1] + change * s < 100:
-                        individual.color_ratios[i][1] += change * s
-                    sum_ratios += individual.color_ratios[r][1]
-
-            individual.normalize()
+def complete(individual: Individual, pm: float) -> None:
+    if random.random() < pm:
+        for i in range(len(individual.color_ratios)):
+            individual.color_ratios[i][1] = mutate_gene(individual.color_ratios[i][1])
+    individual.normalize()
 
 
 mutation_list = {
@@ -64,4 +52,8 @@ mutation_list = {
 
 
 def mutation(config_mutation: int, population: list[Individual], generation: int) -> None:
-    return mutation_list[config_mutation](population, generation)
+    f = mutation_list[config_mutation]
+    
+    pm = 0.4
+    for individual in population:
+        f(individual, pm)
