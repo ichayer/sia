3824add3c39b14ea93361df9f713245a0ca60df1
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2023-04-27 20:13:15 -0300

    Refactor trainer configuration into its own class

--- tp3/src/trainer.py
+++ tp3/src/trainer.py
@@ -2,6 +2,18 @@ import numpy as np
 from .perceptron import Perceptron
 
 
+class TrainerConfig:
+    """Represents a configuration on how how to train a perceptron"""
+    
+    def __init__(self, error_func, acceptable_error, learning_rate=0.1, max_epochs=100, use_batch_increments=False, print_every=None) -> None:
+        self.error_func = error_func
+        self.acceptable_error = acceptable_error
+        self.learning_rate = learning_rate
+        self.max_epochs = max_epochs
+        self.use_batch_increments = use_batch_increments
+        self.print_every = print_every
+
+
 def evaluate_perceptron(perceptron: Perceptron, dataset: list[np.ndarray[float]], dataset_outputs: list[float], error_func, print_output: bool) -> int:
     """
     Evaluates a perceptron with a given dataset.
@@ -18,23 +30,24 @@ def evaluate_perceptron(perceptron: Perceptron, dataset: list[np.ndarray[float]]
     return error_func(dataset_outputs, outputs)
 
 
-def train_perceptron(perceptron: Perceptron, dataset: list[np.ndarray[float]], dataset_outputs: list[float], error_func, acceptable_error=0, learning_rate=0.1, max_epochs=100, use_batch_increments=False, print_every=None):
+def train_perceptron(perceptron: Perceptron, dataset: list[np.ndarray[float]], dataset_outputs: list[float], config: TrainerConfig):
+    print_every = config.print_every
     dataset_with_ones = [np.concatenate(([1], d)) for d in dataset]
 
-    for epoch_idx in range(1, max_epochs+1):
+    for epoch_idx in range(1, config.max_epochs+1):
         for i in range(len(dataset)):
-            perceptron.evaluate_and_adjust(dataset_with_ones[i], dataset_outputs[i], learning_rate)
-            if not use_batch_increments:
+            perceptron.evaluate_and_adjust(dataset_with_ones[i], dataset_outputs[i], config.learning_rate)
+            if not config.use_batch_increments:
                 perceptron.update_weights()
 
         if print_every is not None and epoch_idx % print_every == 0:
             print("--------------------------------------------------")
             print(f"RESULTS AFER EPOCH {epoch_idx} (weights {perceptron.w})")
-        error = evaluate_perceptron(perceptron, dataset, dataset_outputs, error_func, epoch_idx % print_every == 0)
-        if error <= acceptable_error:
+        error = evaluate_perceptron(perceptron, dataset, dataset_outputs, config.error_func, epoch_idx % print_every == 0)
+        if error <= config.acceptable_error:
             break
         
-        if use_batch_increments:
+        if config.use_batch_increments:
             perceptron.update_weights()
 
     if print_every is not None and epoch_idx < print_every:
