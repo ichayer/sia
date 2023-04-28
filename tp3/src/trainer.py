d989a5bfcbb747e48b7df400db15b34112b780e8
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2023-04-28 01:15:22 -0300

    Add end_reason to TrainerResult, add check for when weights haven't changed + configurable epsilon

--- tp3/src/trainer.py
+++ tp3/src/trainer.py
@@ -1,4 +1,5 @@
 import json
+from enum import Enum
 import numpy as np
 from .perceptron import Perceptron
 from . import error_funcs
@@ -7,13 +8,14 @@ from . import error_funcs
 class TrainerConfig:
     """Encapsulates a configuration on how how to train a perceptron."""
     
-    def __init__(self, error_func, acceptable_error, learning_rate=0.1, max_epochs=100, use_batch_increments=False, print_every=None) -> None:
+    def __init__(self, error_func, acceptable_error, learning_rate=0.1, max_epochs=100, use_batch_increments=False, print_every=None, weight_comparison_epsilon=0.00001) -> None:
         self.error_func = error_func
         self.acceptable_error = acceptable_error
         self.learning_rate = learning_rate
         self.max_epochs = max_epochs
         self.use_batch_increments = use_batch_increments
         self.print_every = print_every
+        self.weight_comparison_epsilon = weight_comparison_epsilon
     
     def __init__(self, config_dict):
         self.error_func = error_funcs.map[config_dict['error_func']]
@@ -42,6 +44,7 @@ class TrainerConfig:
             raise Exception(f'Invalid configuration: weight_update_method must be either incremental or batch. Specified: {self.max_epochs}')
         
         self.print_every = int(config_dict['print_every']) if 'print_every' in config_dict else None
+        self.weight_comparison_epsilon = float(config_dict['weight_comparison_epsilon']) if 'weight_comparison_epsilon' in config_dict else None
 
     def from_file(filename: str):
         with open(filename, "r") as f:
@@ -49,13 +52,20 @@ class TrainerConfig:
         return TrainerConfig(config_dict)
 
 
+class EndReason(Enum):
+    EPOCH_LIMIT_REACHED = "Epoch limit reached"
+    ACCEPTABLE_ERROR_REACHED = "Acceptable error reached"
+    WEIGHTS_HAVENT_CHANGED = "Weights haven't changed"
+
+
 class TrainerResult:
     """Encapsulates the result of training a single perceptron."""
     
-    def __init__(self, epoch_num: int, weights_history: list[np.ndarray[float]], error_history: list[float]) -> None:
+    def __init__(self, epoch_num: int, weights_history: list[np.ndarray[float]], error_history: list[float], end_reason: EndReason) -> None:
         self.epoch_num = epoch_num
         self.weights_history = weights_history
         self.error_history = error_history
+        self.end_reason = end_reason
 
 
 def evaluate_perceptron(perceptron: Perceptron, dataset: list[np.ndarray[float]], dataset_outputs: list[float], error_func, print_output: bool) -> int:
@@ -82,8 +92,9 @@ def train_perceptron(perceptron: Perceptron, dataset: list[np.ndarray[float]], d
     epoch_num = 0
     weights_history = [np.copy(perceptron.w)]
     error_history = [error]
+    end_reason = None
 
-    while error > config.acceptable_error and (config.max_epochs is None or epoch_num < config.max_epochs):
+    while error > config.acceptable_error and (config.max_epochs is None or epoch_num < config.max_epochs) and end_reason is None:
         epoch_num += 1
         
         for i in range(len(dataset)):
@@ -99,9 +110,17 @@ def train_perceptron(perceptron: Perceptron, dataset: list[np.ndarray[float]], d
         
         if config.use_batch_increments:
             perceptron.update_weights()
+        if np.subtract(weights_history[-1], perceptron.w).max() < 0.00001:
+            end_reason = EndReason.WEIGHTS_HAVENT_CHANGED
         weights_history.append(np.copy(perceptron.w))
         
         error = evaluate_perceptron(perceptron, dataset, dataset_outputs, config.error_func, print_now)
         error_history.append(error)
 
-    return TrainerResult(epoch_num, weights_history, error_history)
+    if end_reason is None:
+        if error <= config.acceptable_error:
+            end_reason = EndReason.ACCEPTABLE_ERROR_REACHED
+        elif epoch_num == config.max_epochs:
+            end_reason = EndReason.EPOCH_LIMIT_REACHED
+
+    return TrainerResult(epoch_num, weights_history, error_history, end_reason)
