0d18ae6b4588a09f2b2a39dbba55c13080e99510
TomyMarengo <marengotomas@gmail.com>
2023-05-03 01:05:10 -0300

    convergiendo en 30-40 épocas ej3b uwu

--- tp3/main.py
+++ tp3/main.py
@@ -3,31 +3,33 @@ from tp3.src.perceptron import *
 from tp3.src.trainer import train_multilayer_perceptron, TrainerConfig
 from functools import reduce
 
-dataset = [
-    np.array([1, 1]),
-    np.array([1, -1]),
-    np.array([-1, 1]),
-    np.array([-1, -1])
-]
+# Abrir el archivo
+with open("digits.txt", "r") as file:
+    # Leer cada línea del archivo y convertirla en una lista de enteros
+    numbers = [[int(num) for num in line.split()] for line in file]
 
-dataset_outputs_xor = [[-1], [1], [1], [-1]]
+# Convertir la lista de listas en un vector de números
+vector = [num for sublist in numbers for num in sublist]
 
-config = TrainerConfig.from_file("ejercicio3-a-config.json")
+dataset_input = [vector[i:i+35] for i in range(0, len(vector), 35)]
 
-perceptrons_by_layer = [2, 5,  1]
+dataset_outputs = [[1], [-1], [1], [-1], [1], [-1], [1], [-1], [1], [-1]]
+
+config = TrainerConfig.from_file("ejercicio3-b-config.json")
+
+perceptrons_by_layer = [35, 10, 10, 1]
 perceptrons = []
 
 for p in perceptrons_by_layer:
     nl = [0] * p
     perceptrons.append(nl)
 
-
 for i in range(len(perceptrons_by_layer)):
     for j in range(perceptrons_by_layer[i]):
 
         if i == 0:
             perceptrons[i][j] = Perceptron(
-                initial_weights=np.random.random(len(dataset[0]) + 1) * 0.8 - 0.4,
+                initial_weights=np.random.random(len(dataset_input[0]) + 1) * 0.8 - 0.4,
                 theta_func=config.theta
             )
         else:
@@ -36,26 +38,19 @@ for i in range(len(perceptrons_by_layer)):
                 theta_func=config.theta
             )
 
-# for i in range(len(perceptrons_by_layer)):
-#     if i == 0:
-#         weights = np.random.rand(len(dataset[0]) + 1, perceptrons_by_layer[i] + 1) * 2 - 1
-#     else:
-#         weights = np.random.rand(perceptrons_by_layer[i - 1] + 1, perceptrons_by_layer[i] + 1) * 2 - 1
-#
-#     weights_symmetric = (weights + np.flip(weights, axis=0)) / 2
-#
-#     for j in range(perceptrons_by_layer[i]):
-#         perceptrons[i][j] = Perceptron(
-#             initial_weights=weights_symmetric[:, j],
-#             theta_func=config.theta)
+multilayer_perceptron_parity = MultilayerPerceptron(perceptrons)
 
-multilayer_perceptron_xor = MultilayerPerceptron(perceptrons)
-
-result_xor = train_multilayer_perceptron(
-    multilayer_perceptron=multilayer_perceptron_xor,
-    dataset=dataset,
-    dataset_outputs=dataset_outputs_xor,
+result_parity = train_multilayer_perceptron(
+    multilayer_perceptron=multilayer_perceptron_parity,
+    dataset=dataset_input[:8],
+    dataset_outputs=dataset_outputs[:8],
     config=config
 )
 
-print(f"\n\n Epoch: {result_xor.epoch_num} End Reason: {result_xor.end_reason}")
+print(f"\n\n Epoch: {result_parity.epoch_num} End Reason: {result_parity.end_reason}")
+
+
+# evaluate
+
+multilayer_perceptron_parity
+
