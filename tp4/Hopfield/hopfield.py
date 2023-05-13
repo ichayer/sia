c5ab35bdb907ba05cdc3b1d05a4ef634bdf113f1
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2023-05-13 20:33:47 -0300

    Add checks and warnings for hopfields limitations

--- tp4/Hopfield/hopfield.py
+++ tp4/Hopfield/hopfield.py
@@ -2,8 +2,15 @@ import numpy as np
 
 class Hopfield:
     def __init__(self, patterns: np.ndarray) -> None:
+        self.p = len(patterns)
         self.N = len(patterns[0])
         self.weights = np.transpose(patterns) @ patterns / self.N * (1 - np.identity(self.N))
+        if self.p * 20 / 3 > self.N:
+            print('⚠️⚠️⚠️ WARNING: Hopfields algorithm received more patterns than recommended!')
+        for i in range(len(patterns)):
+            for j in range(i):
+                if np.abs(np.dot(patterns[i], patterns[j])) < 0.9:
+                    print(f'⚠️⚠️⚠️ WARNING: Hopfields algorithm non-somewhat-orthogonal patterns at indexes {i} and {j}: {patterns[i]}, {patterns[j]}')
     
     def evaluate(self, query: np.ndarray, max_epochs: int, printer = None):
         if len(query) != self.N:
