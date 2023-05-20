ab14de146d4aa8a31ad72fc9c903ede9fa5c411c
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2023-05-20 18:27:13 -0300

    Add piano and pineapple images for hopfield, plot images instead of emojis

--- tp4/Hopfield/printers.py
+++ tp4/Hopfield/printers.py
@@ -1,5 +1,14 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 def print_letter(s: np.ndarray, charwidth: int, pluschar = '⬛', minuschar = '⬜'):
     for i in range(0, len(s), charwidth):
         print("".join([(pluschar if s[j] > 0 else minuschar) for j in range(i, i + charwidth)]))
+
+def plot_image(s: np.ndarray, size: tuple[int, int]):
+    img = [[] for _ in range(size[1])]
+    for y in range(size[1]):
+        for x in range(size[0]):
+            img[y].append(1 if s[x + y*size[0]] < 0.5 else 0)
+    plt.imshow(img, cmap='gray')
+    return plt
